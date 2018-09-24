fc42df241f09eea2f5fdb18d94bd0d7ee8657956
Mark Rickert <mjar81@gmail.com>
2018-09-24 18:44:43 +0800

    Include project files without motion-require

--- lib/formotion.rb
+++ lib/formotion.rb
@@ -3,11 +3,16 @@ require 'bubble-wrap/core'
 require 'bubble-wrap/font'
 require 'bubble-wrap/camera'
 
-
-
 Motion::Project::App.setup do |app|
-  app.frameworks<<'CoreLocation' unless app.frameworks.include?('CoreLocation')
-  app.frameworks<<'MapKit' unless app.frameworks.include?('MapKit')
-  
+  core_lib = File.join(File.dirname(__FILE__), 'formotion')
+  insert_point = app.files.find_index { |file| file =~ /^(?:\.\/)?app\// } || 0
+
+  Dir.glob(File.join(core_lib, '**/*.rb')).reverse.each do |file|
+    app.files.insert(insert_point, file)
+  end
+
+  app.frameworks << 'CoreLocation' unless app.frameworks.include?('CoreLocation')
+  app.frameworks << 'MapKit' unless app.frameworks.include?('MapKit')
+
   app.resources_dirs << File.join(File.dirname(__FILE__), '../resources')
 end
